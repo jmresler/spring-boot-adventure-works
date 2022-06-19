87df036a311052281edd1758594c0ea6683a0215
John M. Resler <johnmresler@gmail.com>
2022-06-19 18:37:31 -0500

    Building out test frameworks

---
+++ spring-boot-sales-services/src/test/java/org/jmresler/sales/aw/svcs/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.jmresler.sales.aw.svcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-boot-sales-view-services/src/test/java/org/jmresler/sales/aw/views/svcs/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.jmresler.sales.aw.views.svcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
